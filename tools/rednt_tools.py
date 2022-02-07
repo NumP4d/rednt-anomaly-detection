a104bde566451a4034afb9e2bc31a66a63255bb6
Krzysztof Łukomski <krzyluk121@student.polsl.pl>
2022-02-07 04:29:08 +0100

    Added data validity and dropping when it comes to features, added basic feature list extraction and creation of pandas dataframe

--- tools/rednt_tools.py
+++ tools/rednt_tools.py
@@ -8,7 +8,7 @@ import scipy
 from hampel import hampel
 import matplotlib.pyplot as plt
 
-plt.rcParams["figure.figsize"]=50,50
+#plt.rcParams["figure.figsize"]=30,30
 
 DATA_DIR = 'data'
 VIBRATION_FILENAME = os.path.join(DATA_DIR, 'SCUd_134t_drgania.csv')
@@ -19,12 +19,12 @@ SEP = ';'
 SAMPLE_TIME_SEC = 4*60
 
 
-def plot_timeseries_data(df_in):
+def plot_timeseries_data(df_in, style='.'):
     fig, axes = plt.subplots(nrows=2, ncols=1)
     df_in.vibration.plot(ax=axes[0], title='vibrations',
-                         legend=False, style='.')
+                         legend=False, style=style)
     df_in.temperature.plot(ax=axes[1], title='temperature',
-                           legend=False, style='.')
+                           legend=False, style=style)
     plt.show()
 
 
